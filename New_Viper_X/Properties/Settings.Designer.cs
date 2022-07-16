e0c9f54a4c7fe4a6d307ff9a65f5047c94993694
Federation <31602167+discordjsbr@users.noreply.github.com>
2022-07-16 18:21:23 -0300

    Add files via upload

---
+++ New_Viper_X/Properties/Settings.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace New_Viper_X.Properties
+{
+	// Token: 0x02000008 RID: 8
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x1700000B RID: 11
+		// (get) Token: 0x0600007A RID: 122 RVA: 0x0000C166 File Offset: 0x0000A366
+		public static Settings Default
+		{
+			get
+			{
+				return Settings.defaultInstance;
+			}
+		}
+
+		// Token: 0x0400008C RID: 140
+		private static Settings defaultInstance = (Settings)SettingsBase.Synchronized(new Settings());
+	}
+}
